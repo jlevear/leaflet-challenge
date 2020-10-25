200f3a27f2548f47c8def6e6f40935a3580e8b9f
John Levear <jlevear@gmail.com>
2020-10-25 11:19:50 -0700

    adding completed part 1

--- static/js/logic.js
+++ static/js/logic.js
@@ -15,65 +15,99 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 }).addTo(myMap);
 
 // Store data in variable
-let globalData = []
+let globalData = [];
 
 // Grab data with d3
-var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+let url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
 d3.json(url).then(data => {
     globalData = data
-    init(globalData);
-}) 
+    init(globalData)
+});
 
 // Use data to build map layers
-function init(data, index = 0) {
+function init(data) {
     
     // Define a markerSize function that will give each marker a different radius based on its magnitude
     function markerSize(magnitudes) {
-        return magnitudes * 100000;
-    }
-
-    // Define data for map information
-    let features = data.features
-
-    let magnitudes = []
-
-    for (let i = 0; i < features.length; i++) {[
-        mag = features[i].properties.mag,
-        magnitudes.push(mag)
-    ]};
-    console.log(magnitudes)
-
-    let depths = []
-
-    for (let i = 0; i < features.length; i++) {[
-        depth = features[i].geometry.coordinates[2],
-        depths.push(depth)
-    ]};
-    
-    let coordinates = []
-
-    for (let i = 0; i < features.length; i++) {[
-        coordinate = [features[i].geometry.coordinates[1], features[i].geometry.coordinates[0]],
-        coordinates.push(coordinate)
-    ]};
-
-    // Loop through the coordinates array and create one marker for each earthquake
-    for (var i = 0; i < coordinates.length; i++) {
-        L.circle(coordinates[i], {
-            fillOpacity: 0.75,
-            color: "black",
-            fillColor: "purple",
-            // Setting our circle's radius equal to the output of our markerSize function
-            // This will make our marker's size proportionate to its magnitude
-            radius: markerSize(magnitudes[i])
-        }).bindPopup("<h3>Coordinates: " + coordinates[i] + "</h3><h3>Magnitude: " + magnitudes[i] + "</h3>").addTo(myMap);
+        return magnitudes * 50000
     };
 
-        //add date
-        //add location
-        //add coloring
-        //add legend
+    // Define data for map information
+    let features = data.features;
+
+    let magnitudes = [];
+
+    for (let i = 0; i < features.length; i++) {
+        let magnitude = features[i].properties.mag
+        magnitudes.push(magnitude)
+    };
+
+    let depths = [];
+
+    for (let i = 0; i < features.length; i++) {
+        let depth = features[i].geometry.coordinates[2]
+        depths.push(depth)
+    };
+
+    function getColor(d) {
+        return d > 90 ? '#d73027' :
+               d > 70 ? '#fc8d59' :
+               d > 50 ? '#fee08b' :
+               d > 30 ? '#d9ef8b' :
+               d > 10 ? '#91cf60' :
+               d > -10 ? '#1a9850' :
+                        '#1a1a1a';
+    };
+    
+    let coordinates = [];
+
+    for (let i = 0; i < features.length; i++) {
+        let coordinate = [features[i].geometry.coordinates[1], features[i].geometry.coordinates[0]]
+        coordinates.push(coordinate)
+    };
+
+    let locations = [];
+
+    for (let i = 0; i < features.length; i++) {
+        let location = features[i].properties.place
+        locations.push(location)
+    };
+
+    // Create circles based on coordinates
+    // Define radius based on magnitude
+    // Define color based on depth
+    for (let i = 0; i < features.length; i++) {
+        L.circle(coordinates[i], {
+            radius: markerSize(magnitudes[i]),
+            fillColor: getColor(depths[i]),
+            fillOpacity: 1,
+            color: "black",
+            weight: 1            
+        }).bindPopup("<p>Magnitude: " + magnitudes[i] + 
+                    "</p><p>Depth: " + depths[i] + 
+                    "</p><p>Location: " + locations[i] + "</p>").addTo(myMap);
+    };
+
+    // Create legend with depth colors
+    var legend = L.control({position: 'bottomright'});
+
+    legend.onAdd = function (myMap) {
+
+        let div = L.DomUtil.create('div', 'info legend'),
+            grades = [-10, 10, 30, 50, 70, 90];
+
+        for (let i = 0; i < grades.length; i++) {
+            div.innerHTML +=
+                '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+                (grades[i] + 1) + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+        }
+
+        return div;
+    };
+
+    legend.addTo(myMap);
+
 }
 
     
